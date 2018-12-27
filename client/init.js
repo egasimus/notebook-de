6e6e73251437b376466d59a23c49af2ff07e8096
Adam Avramov <adam.b.avramov@gmail.com>
2018-12-27 15:01:06 +0200

    support server state snapshot

--- client/init.js
+++ client/init.js
@@ -5,10 +5,11 @@ import store from '../store'
 export default function init () {
   const xhr = new XMLHttpRequest()
   xhr.open('GET', 'http://localhost:5678')
-  xhr.onload = () => JSON.parse(xhr.responseText).forEach(port=>{
-    const action = Launch(port)
-    store.dispatch(action)
-    connect(port, action.id)
-  })
+  xhr.onload = () => {
+    const commands = JSON.parse(xhr.responseText)
+    Object.keys(commands).forEach(port=>{
+      const action = Launch(commands[port])
+      store.dispatch(action)
+      connect(port, action.id) }) }
   xhr.send()
 }
