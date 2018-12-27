41d13efcd17a8406349984591cd9a8eb1d22ae7a
Adam Avramov <adam.b.avramov@gmail.com>
2018-12-27 18:26:31 +0200

    improve formatting

--- client-web/components/Windows.js
+++ client-web/components/Windows.js
@@ -15,7 +15,11 @@ function Windows ({ timeline, topics }) { return (
   <div className="Workspace">
     {timeline.map(id=>
       <div className="WorkspaceItem" key={id}>
-        <div className="WorkspaceItemTitle">{topics[id].started.toISOString()} {topics[id].command}</div>
+        <div className="WorkspaceItemTitle">
+          <span className="WorkspaceItemTitleDate">{topics[id].started.toISOString().split('T').join(' ').slice(0,-1)} </span>
+          <span>{topics[id].command}</span>
+          <div style={{color:"#8c0"}}>PID <strong>23245</strong>, running</div>
+        </div>
         {(!topics[id].data)
             ? 'waiting...'
             : <XTerm value={topics[id].data.join('')} options={options} />}
